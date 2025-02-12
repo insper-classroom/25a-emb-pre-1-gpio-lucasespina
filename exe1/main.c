29d4ba545c01c21e3f247bad0727e0301b73d364
lucasespina <lucaseqc@al.insper.edu.br>
2025-02-11 21:48:19 -0300

    testando dnv, eu desisto dps dessa

--- exe1/main.c
+++ exe1/main.c
@@ -1,30 +1,31 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
-#include <stdio.h>
-
-const int LED_PIN_R = 5;
-const int LED_PIN_Y = 28;
 
 int main() {
-  stdio_init_all();
+    const int LED_PIN_R = 5;
+    const int LED_PIN_Y = 28;
 
-  gpio_init(LED_PIN_R);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
-
-  gpio_init(LED_PIN_Y);
-  gpio_set_dir(LED_PIN_Y, GPIO_OUT);
-
-  while (true) {
-    gpio_put(LED_PIN_R, 1);
-    gpio_put(LED_PIN_Y, 1);
-
-    sleep_ms(10);
-
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
     gpio_put(LED_PIN_R, 0);
+
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
     gpio_put(LED_PIN_Y, 0);
-    sleep_ms(20);
 
+    while (true) {
+        gpio_put(LED_PIN_R, 1);
+        gpio_put(LED_PIN_Y, 1);
+        sleep_ms(10);  
 
+        gpio_put(LED_PIN_R, 0);
+        gpio_put(LED_PIN_Y, 0);
+        sleep_ms(20);  
 
-  }
+        gpio_put(LED_PIN_R, 1);
+        gpio_put(LED_PIN_Y, 1);
+        sleep_ms(20); 
+    }
+
+    return 0;
 }
